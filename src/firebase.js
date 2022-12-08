49f759b7d4b72ef6e4770ae54fa32e08b0ec75c5
Ayushman Choudhary <ayushman11022002@gmail.com>
2022-12-08 13:56:47 +0530

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKln3ON0IEY0Kc3oCkrZ_-b19JMcixKxE",
+  authDomain: "slack-clone-fe5cc.firebaseapp.com",
+  projectId: "slack-clone-fe5cc",
+  storageBucket: "slack-clone-fe5cc.appspot.com",
+  messagingSenderId: "334523974469",
+  appId: "1:334523974469:web:1c1320a669c955e9bff42c",
+};
