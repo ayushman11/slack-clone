cc93faff9d5adc89cb5e5600cdbf921007efbbdf
Ayushman Choudhary <ayushman11022002@gmail.com>
2022-12-08 14:15:52 +0530

    Header design

---
+++ src/components/Header.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
+import { HelpOutline } from "@mui/icons-material";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      {/* Header Left */}
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTime />
+      </HeaderLeft>
+      {/* Header Search */}
+      <HeaderSearch>
+        <Search />
+        <input placeholder="Search channel, workspace, DM, ..." />
+      </HeaderSearch>
+      {/* Header Right */}
+      <HeaderRight>
+        <HelpOutline />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  width: 100%;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    text-align: center;
+    min-width: 30vw;
+    border: none;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+    flex: 0.3;
+    display: flex;
+    justify-content: flex-end;
+    
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 20px;
+    }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
