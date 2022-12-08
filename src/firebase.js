4cfc4625788f54ddd46b7c01a48c731c4e58b445
Ayushman Choudhary <ayushman11022002@gmail.com>
2022-12-08 17:07:34 +0530

    db & auth settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAKln3ON0IEY0Kc3oCkrZ_-b19JMcixKxE",
   authDomain: "slack-clone-fe5cc.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "334523974469",
   appId: "1:334523974469:web:1c1320a669c955e9bff42c",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
